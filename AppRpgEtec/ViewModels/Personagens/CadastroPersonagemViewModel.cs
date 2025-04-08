d1cfcac462bf59de4bb85985e1b6fd842dab45c8
etechas <etechas@LI02-15>
2025-04-07 22:23:25 -0300

    Add project files.

---
+++ AppRpgEtec/ViewModels/Personagens/CadastroPersonagemViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Services.Personagens;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class CadastroPersonagemViewModel : BaseViewModel
+    {/*(1) Declaração da propriedade que referência a classe de serviço e (2) Criação do 
+      Construtor para
+      receber o objeto que será enviado da View para alimentar esta classe, resgate do 
+       token da aplicação para passar para a instância do serviço.*/
+        //1
+        private PersonagemService pService;
+        //2
+        public CadastroPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+        }
+        private int id;
+        private int inteligencia;
+        private string nome;
+        private int disputas;
+        private int pontosvida;
+        private int vitorias;
+        private int forca;
+        private int derrotas;
+        private int defesa;
+        //Control+R,E Para criar isso 
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnPropertyChanged()
+           ;
+            }
+        }
+        public int Inteligencia
+        {
+            get => inteligencia;
+            set
+            {
+                inteligencia = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Nome { get => nome; set { nome = value; OnPropertyChanged(); } }
+        public int Disputas { get => disputas; set { disputas = value; OnPropertyChanged(); } }
+        public int Pontosvida { get => pontosvida; set { pontosvida = value; OnPropertyChanged(); } }
+        public int Vitorias { get => vitorias; set { vitorias = value; OnPropertyChanged(); } }
+        public int Forca { get => forca; set { forca = value; OnPropertyChanged(); } }
+        public int Derrotas { get => derrotas; set { derrotas = value; OnPropertyChanged(); } }
+        public int Defesa
+        {
+            get => defesa; set
+            {
+                defesa = value; OnPropertyChanged()
+    ;
+            }
+        }
+    }
+}
+
---
+++ AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Services.Personagens;
+using AppRpgEtec.Models;
+using System.Collections.ObjectModel;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+
+        public ObservableCollection<Personagem> Personagens { get; set; }
+
+        public ListagemPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+            Personagens = new ObservableCollection<Personagem>();
+            _ = ObterPersonagens();
+        }
+        //Próximos elementos da classe aqui
+        //Fim da classe
+        public async Task ObterPersonagens()
+        {
+            try //Junto com o Cacth evitará que erros fechem o aplicativo
+            {
+                Personagens = await pService.GetPersonagensAsync();
+                OnPropertyChanged(nameof(Personagens)); //Informará a View que houve carregamento
+
+            }
+            catch (Exception ex)
+            {
+                //Captará o erro para exibir em tela
+                await Application.Current.MainPage
+                .DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+
+            }
+        }
+    }
+}
